cd0b30871970129738fd9c7edd425af8d6aaae83
LazlowHUN <doczilaci1111@gmail.com>
2026-03-12 13:50:49 +0100

    ETL tool db to db first version for test

--- dags/load_kafka_to_dwh.py
+++ dags/load_kafka_to_dwh.py
@@ -1,8 +1,5 @@
-from airflow.providers.apache.kafka.hooks.consume import KafkaConsumerHook
-from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
 from airflow.sdk import dag, task, task_group
 from datetime import datetime
-from airflow.providers.oracle.hooks.oracle import OracleHook
 
 @dag(
     start_date=datetime(2026, 3, 10),
@@ -11,14 +8,15 @@ from airflow.providers.oracle.hooks.oracle import OracleHook
 )
 def load_kafka_to_oracle():
 
-    @task
-    def read_from_kafka():
-        return None
-    
-    @task
-    def load_to_oracle():
-        return None
-    
-    read_from_kafka >> load_to_oracle
+    from module.database_functions import load_db_to_db
 
+    LOAD_ORACLE_TO_ORACLE = load_db_to_db.override(task_id="LOAD_ORACLE_TO_ORACLE")(
+        source_engine="",
+        source_table="EMPLOYEES",
+        target_engine="",
+        target_schema="DWH",
+        target_table="DIM_EMP",
+        chunksize=5000
+    )
+    
 load_kafka_to_oracle()
